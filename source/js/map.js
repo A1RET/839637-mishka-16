b8057a3c80048aad77de3afc5d365983cd3ddfdf
Airet <thiefon@gmail.com>
2019-05-01 01:03:21 +0500

    Исправил одинарные кавычки на двойные

--- source/js/map.js
+++ source/js/map.js
@@ -10,15 +10,15 @@ function init(){
     controls: []
   });
 
-  myMap.behaviors.disable('scrollZoom');
+  myMap.behaviors.disable("scrollZoom");
 
   myMap.controls.add("zoomControl", {
     position: {top: 30, right: 15}
   });
 
   var myPlacemark = new ymaps.Placemark([59.938631, 30.323055] , {},
-    { iconLayout: 'default#image',
-      iconImageHref: 'img/decor/icon-map-pin.svg',
+    { iconLayout: "default#image",
+      iconImageHref: "img/decor/icon-map-pin.svg",
       iconImageSize: [67, 100],
       iconImageOffset: [-24, -82]
     });
