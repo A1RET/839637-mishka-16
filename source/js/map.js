dc262339185d1c359e146a487b4b1701c47fa7b0
Airet <thiefon@gmail.com>
2019-04-30 20:08:14 +0500

    добавил карту с кастомной меткой

---
+++ source/js/map.js
@@ -0,0 +1,27 @@
+ymaps.ready(init);
+
+function init(){
+
+  var myMap;
+
+  myMap = new ymaps.Map("map", {
+    center: [59.938631, 30.323055],
+    zoom: 17,
+    controls: []
+  });
+
+  myMap.behaviors.disable('scrollZoom');
+
+  myMap.controls.add("zoomControl", {
+    position: {top: 30, right: 15}
+  });
+
+  var myPlacemark = new ymaps.Placemark([59.938631, 30.323055] , {},
+    { iconLayout: 'default#image',
+      iconImageHref: '../img/decor/icon-map-pin.svg',
+      iconImageSize: [67, 100],
+      iconImageOffset: [-24, -82]
+    });
+
+  myMap.geoObjects.add(myPlacemark);
+}
